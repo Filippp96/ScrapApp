6d782309d787f46bd389c4b5e4edfa641f048a90
filip <filipkk96@gmail.com>
2025-05-05 20:57:35 +0200

    MongoDB backup policies changed

--- main.tf
+++ main.tf
@@ -147,7 +147,7 @@ resource "azurerm_postgresql_flexible_server" "main" {
 
 // ---== MongoDB ==---
 resource "azurerm_cosmosdb_account" "main" {
-  name                          = "mongo-account-scrap-app"
+  name                          = "mongodb-scrap-app"
   location                      = azurerm_resource_group.main.location
   resource_group_name           = azurerm_resource_group.main.name
   offer_type                    = "Standard"
@@ -168,6 +168,11 @@ resource "azurerm_cosmosdb_account" "main" {
   capabilities {
     name = "EnableMongo"
   }
+
+  backup {
+    type = "Continuous"
+    tier = "Continuous7Days"
+  }
 }
 
 resource "azurerm_cosmosdb_mongo_database" "main" {
@@ -183,7 +188,12 @@ resource "azurerm_cosmosdb_mongo_collection" "main" {
   account_name        = azurerm_cosmosdb_account.main.name
   database_name       = azurerm_cosmosdb_mongo_database.main.name
 
-  default_ttl_seconds = 777
-  shard_key           = "_id"
+  default_ttl_seconds = "777"
+  shard_key           = "mongodbScrapAppKey"
   throughput          = 400
+
+  index {
+    keys   = ["_id"]
+    unique = true
+  }
 }
