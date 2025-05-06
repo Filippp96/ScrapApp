315c6fc55af8a5c22ed0006f2574592f0433a38f
filip <filipkk96@gmail.com>
2025-05-06 15:58:36 +0200

    Outputs changed

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,6 @@
-output "postgresql_password" {
-  value     = azurerm_key_vault_secret.postgresql_password.value
+/*
+output "aaa" {
+  value     = ...
   sensitive = true
 }
-
-output "postgresql_login" {
-  value     = azurerm_key_vault_secret.postgresql_login.value
-  sensitive = true
-}
-
-output "mongodb_password" {
-  value     = azurerm_key_vault_secret.mongodb_password.value
-  sensitive = true
-}
-
-output "mongodb_login" {
-  value     = azurerm_key_vault_secret.mongodb_login.value
-  sensitive = true
-}
+*/
