670ac773737c93de06e00a8c9464b21e9c072919
filip <filipkk96@gmail.com>
2025-05-06 20:01:46 +0200

    IP allocation method changed

--- virtualmachines.tf
+++ virtualmachines.tf
@@ -16,7 +16,7 @@ resource "azurerm_public_ip" "main" {
   name                = "scrapAppPIP"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 }
 
 resource "azurerm_network_interface" "main" {
@@ -26,7 +26,7 @@ resource "azurerm_network_interface" "main" {
 
   ip_configuration {
     name                          = "internal"
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
     subnet_id                     = azurerm_subnet.main.id
     public_ip_address_id          = azurerm_public_ip.main.id
   }
